cd661426c26a765e692a9cd78c8befa29494efbe
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-08-13 21:48:46 +0100

    Add new solution to task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,33 +4,27 @@ Function that queries the Reddit API and prints
 the top ten hot posts of a subreddit
 """
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
+    """Request top ten hot posts of subreddit
+    from Reddit API
+    """
+    # set custom user-agent
+    user_agent = '0x16-api_advanced-jmajetich'
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
 
-    headers = {
-        'User-Agent': u_agent
-    }
+    # custom user-agent avoids request limit
+    headers = {'User-Agent': user_agent}
 
-    params = {
-        'limit': 10
-    }
+    r = requests.get(url, headers=headers, allow_redirects=False)
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
-    if res.status_code != 200:
-        print(None)
-        return
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    if len(hot_posts) is 0:
-        print(None)
+    if r.status_code != 200:
+        print('None')
     else:
-        for post in hot_posts:
+        # load response unit from json
+        data = r.json()['data']
+        # extract list of pages
+        posts = data['children']
+        for post in posts:
             print(post['data']['title'])
